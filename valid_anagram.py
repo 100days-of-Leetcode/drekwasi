77cbd64d3428fae98a3acd419efbbff3d54bcead
DreKwasi <andrewsboateng137@gmail.com>
2024-08-24 20:44:06 +0000

    neetcode solution

--- valid_anagram.py
+++ valid_anagram.py
@@ -1,3 +1,10 @@
+"""
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+"""
+
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
@@ -6,17 +13,12 @@ class Solution:
         track_s = {}
         track_t = {}
 
-        for char in s:
-            if char not in t:
+        for i in range(len(s)):
+            if s[i] not in t:
                 return False
-            track_s[char] = 1
-            if track_s.get(char):
-                track_s[char] += 1
 
-        for char in t:
-            track_t[char] = 1
-            if track_t.get(char):
-                track_t[char] += 1
+            track_s[s[i]] = 1 + track_s.get(s[i], 0)
+            track_t[t[i]] = 1 + track_t.get(t[i], 0)
 
         for key in track_s.keys():
             if track_s[key] != track_t.get(key):
