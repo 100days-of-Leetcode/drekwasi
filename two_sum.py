e0352144b0ab396c84eae6a966cc9f4492d96e1c
DreKwasi <andrewsboateng137@gmail.com>
2024-08-24 21:44:21 +0000

    one pass solution for two sum

--- two_sum.py
+++ two_sum.py
@@ -18,3 +18,16 @@ class Solution:
             for j in range(i + 1, length_nums):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+# One pass solution
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        length_nums = len(nums)
+        if length_nums == 2:
+            return [0, 1]
+        prevMap = {}
+        for index, num in enumerate(nums):
+            diff = target - num
+            if diff in prevMap:
+                return [index, prevMap[diff]]
+            prevMap[num] = index
