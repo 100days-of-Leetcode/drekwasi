f39280441d804ba4c28d294cab8b2453b775b852
DreKwasi <andrewsboateng137@gmail.com>
2024-08-28 00:37:13 +0000

    O(n) solution

--- top_k_frequent_elements.py
+++ top_k_frequent_elements.py
@@ -2,6 +2,8 @@
 Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
 """
 
+from typing import List
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counts = {}
@@ -12,3 +14,21 @@ class Solution:
         sorted_counts = sorted(counts.items(), key=lambda x: x[1], reverse=True)
 
         return [sorted_counts[x][0] for x in range(0, k)]
+
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for x in range(len(nums) + 1)]
+
+        for n in nums:
+            count[n] = count.get(n, 0) + 1
+        
+        for k, v in count.items():
+            freq[v].append(k)
+        
+        result = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                result.append(n)
+                if len(result) == k:
+                    return result
