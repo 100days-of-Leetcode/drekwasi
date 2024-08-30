da719deef8c96aca565c285dd52212e9e5490386
DreKwasi <andrewsboateng137@gmail.com>
2024-08-30 12:57:20 +0000

    two pointer solution

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -21,3 +21,20 @@ class Solution:
                 water_vol.append(min_height * distance)
         
         return max(water_vol)
+
+
+# Two pointer solution
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        l, r = 0, len(height) - 1
+        max_area = 0
+        while l < r:
+            max_area = max(max_area, min(height[l], height[r]) * (r - l))
+            if height[l] > height[r]:
+                r -= 1
+            elif height[r] > height[l]:
+                l += 1
+            else:
+                l += 1
+        
+        return max_area
