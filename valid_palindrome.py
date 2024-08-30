e3603d9bfc0d5b793515a949fdb849f54ee87a4e
DreKwasi <andrewsboateng137@gmail.com>
2024-08-30 11:45:22 +0000

    two pointer solution

--- valid_palindrome.py
+++ valid_palindrome.py
@@ -12,3 +12,29 @@ class Solution:
                 clean_s += i.lower()
 
         return clean_s == clean_s[::-1]
+
+# Two pointer solution
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+
+        while l < r:
+            while l < r and not self.isAlphaNum(s[l]):
+                l += 1
+            while r > l and not self.isAlphaNum(s[r]):
+                r -= 1
+
+            if s[l].lower() != s[r].lower():
+                return False
+            
+            l, r = l+1, r-1
+        
+        return True
+
+    def isAlphaNum(self, c):
+        if(( ord('A') <= ord(c) <= ord('Z')) or 
+            (ord('a') <= ord(c) <= ord('z')) or
+            (ord('0') <= ord(c) <= ord('9'))
+        ):
+            return True
+        
